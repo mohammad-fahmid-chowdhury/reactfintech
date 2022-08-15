b4a2b534af5e629d6b1d90dbc7e629ef025ef38e
mohammad-fahmid-chowdhury <111264270+mohammad-fahmid-chowdhury@users.noreply.github.com>
2022-08-15 10:40:16 +0100

    Hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,8 +16,8 @@ const Hero = () => {
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+            Made By <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Mohammed</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
+          Fahmid Chowdhury
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
